c285e09aeea99c820c826045d51386478d4eccb8
Michal-Wrona <michalwro112@gmail.com>
2022-02-21 13:36:11 +0100

    stworzenie Db context

---
+++ LibraryWeb.Core/DbAccess/LibraryWebDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LibraryWeb.Core.DbAccess
+{
+    public class LibraryWebDbContext : DbContext
+    {
+        public LibraryWebDbContext(DbContextOptions options) : base(options) 
+        { }
+    }
+}
