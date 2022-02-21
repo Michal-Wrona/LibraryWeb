bdce3eb6a1e084d9e1d092afad8094ee03047c7d
Michal-Wrona <michalwro112@gmail.com>
2022-02-21 14:36:07 +0100

    podłączenie do bazy danych dla db contextu

--- LibraryWeb/Startup.cs
+++ LibraryWeb/Startup.cs
@@ -1,3 +1,4 @@
+using LibraryWeb.Core.DbAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace LibraryWeb
 {
@@ -26,6 +29,7 @@ namespace LibraryWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<LibraryWebDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("LibraryWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
