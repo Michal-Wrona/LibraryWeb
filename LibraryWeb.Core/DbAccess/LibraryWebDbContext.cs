e8d207cbe05c5d73b8ffb2ba07e6027df6d3be0f
Michal-Wrona <michalwro112@gmail.com>
2022-02-25 10:56:54 +0100

    dodanie Person do DbContext

--- LibraryWeb.Core/DbAccess/LibraryWebDbContext.cs
+++ LibraryWeb.Core/DbAccess/LibraryWebDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibraryWeb.Core.Persons;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,5 +11,6 @@ namespace LibraryWeb.Core.DbAccess
     {
         public LibraryWebDbContext(DbContextOptions options) : base(options) 
         { }
+        public DbSet<Person> Persons { get; set; }
     }
 }
