2907dfb01217df20bcd3a49d76581d1787db2a54
Michal-Wrona <michalwro112@gmail.com>
2022-02-25 11:05:38 +0100

    Dodanie AddPersonDto i GetPersonReturnDto w LibraryWeb.Api

---
+++ LibraryWeb.Api/Persons/AddPersonDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryWeb.Api.Persons
+{
+    public class AddPersonDto
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string MailAddress { get; set; }
+    }
+}
---
+++ LibraryWeb.Api/Persons/GetPersonReturnDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryWeb.Api.Persons
+{
+    public class GetPersonReturnDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string MailAddress { get; set; }
+    }
+}
