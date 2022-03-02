6a2adc4be5eb85addec47444afcae39fe4120fc6
Michal-Wrona <michalwro112@gmail.com>
2022-03-02 11:05:13 +0100

    Dodanie GetPersons i AddPerson w PersonController

--- LibraryWeb/Controllers/PersonController.cs
+++ LibraryWeb/Controllers/PersonController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LibraryWeb.Api.Persons;
+using LibraryWeb.Core.Persons;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace LibraryWeb.Controllers
@@ -11,5 +14,44 @@ namespace LibraryWeb.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonController (IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        [HttpGet]
+        public ActionResult <IList<GetPersonReturnDto>> GetPersons([FromQuery] string name, [FromQuery] string surname)
+        {
+            var persons = _personRepository.GetPersons();
+            var personsList = new List<GetPersonReturnDto>();
+
+            foreach (var person in persons.Where(x => string.Equals(x.Name, name, StringComparison.CurrentCultureIgnoreCase) && string.Equals (x.Surname, surname, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                personsList.Add(new GetPersonReturnDto()
+                {
+                    Id = person.Id,
+                    MailAddress = person.MailAddress,
+                    Name = person.Name,
+                    Surname = person.Surname
+                });
+
+
+            }
+
+            return Ok(personsList);
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(AddPersonDto dto)
+        {
+            var person = new Person { Name = dto.Name, Surname = dto.Surname, MailAddress = dto.MailAddress };
+
+            _personRepository.AddPerson(person);
+
+            return Ok();
+
+        }
     }
 }
